9951b81b6781eb9739ba0a174a2e12203a8e9155
baz <bazzadobs@live.co.uk>
2023-11-13 23:02:47 +0000

    Incremental changes

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 
 namespace osumatchresults
@@ -11,8 +12,8 @@ namespace osumatchresults
     {
         static async System.Threading.Tasks.Task Main(string[] args)
         {
-            Console.Write("API KEY: ");
-            var apikey = Console.ReadLine();
+            //Console.Write("API KEY: ");
+            //var apikey = Console.ReadLine();
 
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("https://osu.ppy.sh/api/");
@@ -21,89 +22,116 @@ namespace osumatchresults
 
             int matchid = 0;
 
-            while (matchid == 0)
+
+
+            Console.Write("MATCH ID: ");
+
+            var input = Console.ReadLine();
+            var inputs = input.Split(" ");
+            foreach(var varname in inputs)
             {
-                Console.Write("MATCH ID: ");
-                bool success = int.TryParse(Console.ReadLine(), out matchid);
+                bool success = int.TryParse(varname, out matchid);
                 if (!success)
                 {
                     Console.WriteLine("Please enter a valid id");
+                    continue;
                 }
-            }
 
-            String responseString = await client.GetStringAsync(client.BaseAddress + "get_match?k=" + apikey + "&mp=" + matchid);
-            var json = JObject.Parse(responseString);
-            var lobbyname = Regex.Replace(json["match"]["name"].ToString(), "[^A-Za-z0-9 -]", "");
-            Console.WriteLine(lobbyname);
-            List<Game> games = new List<Game>();
+                String responseString = await client.GetStringAsync(client.BaseAddress + "get_match?k=" + apikey + "&mp=" + matchid);
+                var json = JObject.Parse(responseString);
+                var lobbyname = Regex.Replace(json["match"]["name"].ToString(), "[^A-Za-z0-9 -]", "");
+                Console.WriteLine(lobbyname);
+                List<Game> games = new List<Game>();
 
-            foreach (var game in json["games"])
-            {
-                Game newGame = new Game();
-                newGame.scores = new List<Score>();
-                newGame.beatmapid = int.Parse(game["beatmap_id"].ToString());
-
-                String beatmapResponse = await client.GetStringAsync(client.BaseAddress + "get_beatmaps?k=" + apikey + "&b=" + newGame.beatmapid);
-                beatmapResponse = beatmapResponse.Substring(1, beatmapResponse.Length - 2);
-                var beatmapjson = JObject.Parse(beatmapResponse);
-                newGame.beatmap = beatmapjson["artist"] + " " + beatmapjson["title"] + " [" + beatmapjson["version"] + "]";
-
-                Console.WriteLine(newGame.beatmapid + " " + newGame.beatmap);
-
-                foreach (var score in game["scores"])
+                foreach (var game in json["games"])
                 {
-                    Score newScore = new Score();
+                    Game newGame = new Game();
+                    newGame.scores = new List<Score>();
+                    newGame.beatmapid = int.Parse(game["beatmap_id"].ToString());
 
-                    newScore.userid = int.Parse(score["user_id"].ToString());
-
-                    String userResponse = await client.GetStringAsync(client.BaseAddress + "get_user?k=" + apikey + "&u=" + newScore.userid);
-                    userResponse = userResponse.Substring(1, userResponse.Length - 2);
-                    var userJson = JObject.Parse(userResponse);
-                    newScore.user = userJson["username"].ToString();
-
-
-                    newScore.score = int.Parse(score["score"].ToString());
-
-                    Mods mods;
-
-                    if (int.Parse(game["mods"].ToString()) == 0)
+                    if (newGame.beatmapid == 3860999)
                     {
-                        mods = (Mods)int.Parse(score["enabled_mods"].ToString());
-                    }
-                    else
-                    {
-                        mods = (Mods)int.Parse(game["mods"].ToString());
+                        newGame.beatmapid = 3861000;
                     }
 
-                    foreach (Mods flagToCheck in Mods.GetValues(typeof(Mods)))
+                    String beatmapResponse = await client.GetStringAsync(client.BaseAddress + "get_beatmaps?k=" + apikey + "&b=" + newGame.beatmapid);
+                    beatmapResponse = beatmapResponse.Substring(1, beatmapResponse.Length - 2);
+                    var beatmapjson = JObject.Parse(beatmapResponse);
+                    newGame.beatmap = beatmapjson["artist"] + " " + beatmapjson["title"] + " [" + beatmapjson["version"] + "]";
+                    newGame.beatmapsetid = int.Parse(beatmapjson["beatmapset_id"].ToString());
+
+                    Console.WriteLine(newGame.beatmapid + " " + newGame.beatmap);
+
+                    foreach (var score in game["scores"])
                     {
-                        if (mods.HasFlag(flagToCheck))
+                        Score newScore = new Score();
+
+                        newScore.matchid = matchid;
+
+                        var userid = int.Parse(score["user_id"].ToString());
+
+                        String userResponse = await client.GetStringAsync(client.BaseAddress + "get_user?k=" + apikey + "&u=" + userid);
+                        userResponse = userResponse.Substring(1, userResponse.Length - 2);
+
+                        if (userResponse == "")
                         {
-                            newScore.mods = newScore.mods + flagToCheck.ToString();
+                            newScore.user = "Fia";
                         }
+                        else
+                        {
+                            var userJson = JObject.Parse(userResponse);
+                            newScore.user = userJson["username"].ToString();
+                        }
+
+                        newScore.score = int.Parse(score["score"].ToString());
+
+                        Mods mods;
+
+                        if (int.Parse(game["mods"].ToString()) == 0)
+                        {
+                            mods = (Mods)int.Parse(score["enabled_mods"].ToString());
+                        }
+                        else
+                        {
+                            mods = (Mods)int.Parse(game["mods"].ToString());
+                        }
+
+                        foreach (Mods flagToCheck in Mods.GetValues(typeof(Mods)))
+                        {
+                            if (mods.HasFlag(flagToCheck))
+                            {
+                                newScore.mods = newScore.mods + flagToCheck.ToString();
+                            }
+                        }
+
+                        newScore.acc = ((int.Parse(score["count300"].ToString()) * 300.0f) + (int.Parse(score["count100"].ToString()) * 100.0f) + (int.Parse(score["count50"].ToString()) * 50.0f)) / ((int.Parse(score["count300"].ToString()) + int.Parse(score["count100"].ToString()) + int.Parse(score["count50"].ToString()) + int.Parse(score["countmiss"].ToString())) * 300.0f);
+
+                        newScore.count300 = int.Parse(score["count300"].ToString());
+                        newScore.count100 = int.Parse(score["count100"].ToString());
+                        newScore.count50 = int.Parse(score["count50"].ToString());
+                        newScore.countmiss = int.Parse(score["countmiss"].ToString());
+                        newScore.run = "";
+
+                        newGame.scores.Add(newScore);
                     }
 
-                    newScore.acc = ((int.Parse(score["count300"].ToString()) * 300.0f) + (int.Parse(score["count100"].ToString()) * 100.0f) + (int.Parse(score["count50"].ToString()) * 50.0f)) / ((int.Parse(score["count300"].ToString()) + int.Parse(score["count100"].ToString()) + int.Parse(score["count50"].ToString()) + int.Parse(score["countmiss"].ToString())) * 300.0f);
-
-                    newGame.scores.Add(newScore);                    
+                    games.Add(newGame);
                 }
 
-                games.Add(newGame);
-            }
+                StreamWriter file = new(lobbyname + ".xls", append: true);
 
-            StreamWriter file = new(lobbyname + ".csv", append: true);
-
-            foreach (var game in games)
-            {
-                await file.WriteLineAsync(game.beatmapid + "," + game.beatmap);
-
-                foreach (var score in game.scores)
+                foreach (var game in games)
                 {
-                    await file.WriteLineAsync(score.user + "," + score.score + "," + score.acc + "," + score.mods);
-                }
-            }
+                    //await file.WriteLineAsync(game.beatmapid + "," + game.beatmap);
 
-            await file.FlushAsync();
+                    foreach (var score in game.scores)
+                    {
+                        await file.WriteLineAsync(game.beatmapsetid + "," + score.user + "," + score.score + "," + score.acc + "," + "1st" + "," + score.mods + "," + score.count300 + "," + score.count100 + "," + score.count50 + "," + score.countmiss);
+                    }
+                }
+
+                await file.FlushAsync();
+            }
         }
     }
 
@@ -111,16 +139,22 @@ namespace osumatchresults
     {
         public List<Score> scores;
         public int beatmapid;
+        public int beatmapsetid;
         public string beatmap;
     }
 
     public struct Score
     {
-        public int userid;
+        public int matchid;
         public string user;
         public int score;
-        public string mods;
         public float acc;
+        public string run;
+        public string mods;
+        public int count300;
+        public int count100;
+        public int count50;
+        public int countmiss;
     }
 
     [Flags]
